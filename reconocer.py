00c1cbb51431674a7363562faec66a4f719d9076
natalia <marianataliagonzalez.ar@gmail.com>
2019-08-25 19:06:04 -0300

    Comentarios al codigo 3

--- reconocer.py
+++ reconocer.py
@@ -1,6 +1,7 @@
 import cv2
 import train, detect, config, imutils, argparse
 
+# Funcion que reconoce imagen pasada por parametro
 def RecognizeFace(image, faceCascade, eyeCascade, faceSize, threshold):
     found_faces = []
     recognizer = train.trainRecognizer("train", faceSize, showFaces=True)
@@ -24,19 +25,24 @@ if __name__ == '__main__':
     threshold = 500
     recognizer = train.trainRecognizer('train', faceSize, showFaces=True)
 
+    # Crea la ventana con el nombre 'Reconocimiento Facial' y la imagen a reconocer
     cv2.namedWindow("Reconocimiento Facial", 1)
     capture = cv2.imread(args["image"])
 
+    # Inicia bucle
     while True:
         img = imutils.resize(capture, height=500)
+        # Busca el nombre de la persona del rostro que esta en la imagen
         for (label, confidence, (x, y, w, h)) in RecognizeFace(img, faceCascade, eyeCascade, faceSize, threshold):
             font = cv2.FONT_HERSHEY_DUPLEX
+            # Coloca rectangulo en el rostro encontrado y reconocido
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            # Coloca el nombre de la persona reconocida
             cv2.putText(img, "{}".format(recognizer.getLabelInfo(label)), (x, y-5), font, 1, (0,255,0), 1, cv2.LINE_AA)
 
-        # Indico el nombre de la persona reconocida en la imagen pasada por parámetro
+        # Indica el nombre de la persona reconocida en la imagen pasada por parámetro
         print("Rostro Reconocido: %s" % (recognizer.getLabelInfo(label)))
-        # Creo la ventana con el nombre 'Reconocimiento Facial' y la imagen a reconocer
+        # Crea la ventana con el nombre 'Reconocimiento Facial' y la imagen a reconocer
         cv2.imshow("Reconocimiento Facial", img)
 
         # Comprueba si se ha pulsado la tecla 'Esc' para salir del bucle
